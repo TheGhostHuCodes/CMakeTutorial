2c20daf5948ffb22d59d16be9d6532f91bd55842
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-03-23 17:25:07 -0700

    Rewrite helloString lambda as a std::function.

--- helloWorld/tests/Hello2/main.cpp
+++ helloWorld/tests/Hello2/main.cpp
@@ -1,10 +1,12 @@
 /* Hello World program */
 #include <iostream>
+#include <functional>
 
 int main(int argc, char** argv)
 {
-    auto helloString = [](){
-        return "HelloWorld!\n";
+    typedef std::function<const char* const(void)> StringFunction_t;
+    StringFunction_t helloString = [](){
+        return "Hello World!\n";
     };
     std::cout << helloString();
     return 0;
