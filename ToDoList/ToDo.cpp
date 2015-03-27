89c0b451b0cf8ddb8a86de6c90f059b8ffa20147
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-03-27 07:02:22 -0700

    Add ToDoList implementation and test.

--- ToDoList/ToDo.cpp
+++ ToDoList/ToDo.cpp
@@ -1,7 +1,17 @@
 #include "ToDo.hpp"
 
-ToDo::ToDo() {
-}
+ToDo::ToDo() {}
 
-ToDo::~ToDo() {
+ToDo::~ToDo() {}
+
+size_t ToDo::size() const { return this_tasks.size(); }
+
+void ToDo::addTask(const std::string& task) { this_tasks.push_back(task); }
+
+std::string ToDo::getTask(size_t index) const {
+    if (index < this_tasks.size()) {
+        return this_tasks[index];
+    } else {
+        return "";
+    }
 }
--- ToDoList/ToDo.hpp
+++ ToDoList/ToDo.hpp
@@ -1,10 +1,20 @@
 #ifndef TODO_HPP
 #define TODO_HPP
 
+#include <string>
+#include <vector>
+
 class ToDo {
-public:
+  public:
     ToDo();
     ~ToDo();
+
+    size_t size() const;
+    void addTask(const std::string& task);
+    std::string getTask(size_t index) const;
+
+  private:
+    std::vector<std::string> this_tasks;
 };
 
 #endif // TODO_HPP
