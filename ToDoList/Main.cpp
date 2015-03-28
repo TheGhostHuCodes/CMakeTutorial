e88ba1066150d115f6f9eee01e99c26727a73201
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-03-27 18:16:39 -0700

    Apply Mozilla style clang-format.

--- ToDoList/Main.cpp
+++ ToDoList/Main.cpp
@@ -6,7 +6,7 @@ using std::endl;
 #include "ToDoCore/ToDo.hpp"
 
 #define EXPECT_EQUAL(test, expect)                                             \
-    equalityTest(test, expect, #test, #expect, __FILE__, __LINE__)
+  equalityTest(test, expect, #test, #expect, __FILE__, __LINE__)
 
 template <typename T1, typename T2>
 int equalityTest(const T1 testValue, const T2 expectedValue,
@@ -14,36 +14,32 @@ int equalityTest(const T1 testValue, const T2 expectedValue,
                  const char* fileName, const int lineNumber);
 
 int main(int, char**) {
-    int result = 0;
-
-    ToDo list;
-
-    list.addTask("write code");
-    list.addTask("compile");
-    list.addTask("test");
-
-    result |= EXPECT_EQUAL(list.size(), size_t(3));
-    result |= EXPECT_EQUAL(list.getTask(0), "write code");
-    result |= EXPECT_EQUAL(list.getTask(1), "compile");
-    result |= EXPECT_EQUAL(list.getTask(2), "test");
-
-    if (result == 0) {
-        cout << "Test passed" << endl;
-    }
-    return result;
+  int result = 0;
+  ToDo list;
+  list.addTask("write code");
+  list.addTask("compile");
+  list.addTask("test");
+  result |= EXPECT_EQUAL(list.size(), size_t(3));
+  result |= EXPECT_EQUAL(list.getTask(0), "write code");
+  result |= EXPECT_EQUAL(list.getTask(1), "compile");
+  result |= EXPECT_EQUAL(list.getTask(2), "test");
+  if (result == 0) {
+    cout << "Test passed" << endl;
+  }
+  return result;
 }
 
 template <typename T1, typename T2>
 int equalityTest(const T1 testValue, const T2 expectedValue,
                  const char* testName, const char* expectedName,
                  const char* fileName, const int lineNumber) {
-    if (testValue != expectedValue) {
-        cerr << fileName << ":" << lineNumber << ": "
-             << "Expected " << testName << " "
-             << "to equal " << expectedName << " (" << expectedValue << ") "
-             << "but it was (" << testValue << ")" << endl;
-        return 1;
-    } else {
-        return 0;
-    }
+  if (testValue != expectedValue) {
+    cerr << fileName << ":" << lineNumber << ": "
+         << "Expected " << testName << " "
+         << "to equal " << expectedName << " (" << expectedValue << ") "
+         << "but it was (" << testValue << ")" << endl;
+    return 1;
+  } else {
+    return 0;
+  }
 }
--- ToDoList/ToDoCore/ToDo.hpp
+++ ToDoList/ToDoCore/ToDo.hpp
@@ -5,16 +5,16 @@
 #include <vector>
 
 class ToDo {
-  public:
-    ToDo();
-    ~ToDo();
+public:
+  ToDo();
+  ~ToDo();
 
-    size_t size() const;
-    void addTask(const std::string& task);
-    std::string getTask(size_t index) const;
+  size_t size() const;
+  void addTask(const std::string& task);
+  std::string getTask(size_t index) const;
 
-  private:
-    std::vector<std::string> this_tasks;
+private:
+  std::vector<std::string> this_tasks;
 };
 
 #endif // TODO_HPP
