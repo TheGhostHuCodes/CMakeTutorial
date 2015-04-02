243eb01e91d098acbfc2410804d8c928718d495b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-04-01 17:47:36 -0700

    Clean up source with clang-format.

--- CMakeEnvironment/Libraries/Math/Source/Vector3.cpp
+++ CMakeEnvironment/Libraries/Math/Source/Vector3.cpp
@@ -2,9 +2,4 @@
 
 using namespace Math;
 
-Vector3f::Vector3f(float x, float y, float z)
-    : mX(x)
-    , mY(y)
-    , mZ(z)
-{
-}
+Vector3f::Vector3f(float x, float y, float z) : mX(x), mY(y), mZ(z) {}
